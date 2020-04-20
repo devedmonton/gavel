0bca1cc20b6169d5b133b50912ee1e69c43b8052
Aaron Yong <a.yong91@gmail.com>
2020-04-19 21:49:41 -0600

    Export YouTube URL in CSV

--- gavel/controllers/api.py
+++ gavel/controllers/api.py
@@ -8,12 +8,13 @@ from flask import Response
 @utils.requires_auth
 def item_dump():
     items = Item.query.order_by(desc(Item.mu)).all()
-    data = [['Mu', 'Sigma Squared', 'Name', 'Location', 'Description', 'Active']]
+    data = [['Mu', 'Sigma Squared', 'Name', 'URL', 'YouTube URL', 'Description', 'Active']]
     data += [[
         str(item.mu),
         str(item.sigma_sq),
         item.name,
         item.location,
+        item.youtube_url,
         item.description,
         item.active
     ] for item in items]
